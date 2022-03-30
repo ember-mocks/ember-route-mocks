f253137470ee9288f4b44a7674195ae02652dff8
Ali Razfar <arazfar@linkedin.com>
2022-03-30 14:46:20 -0700

    set deprecated jquery-integration optional feature flag to false

--- config/ember-try.js
+++ config/ember-try.js
@@ -51,7 +51,7 @@ module.exports = async function () {
         name: 'ember-default-with-jquery',
         env: {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
+            'jquery-integration': false,
           }),
         },
         npm: {
